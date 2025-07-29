b837d31d5856ce4d6eedb974813177ada5186f6c
igtaposh <debnathtaposh58@gamil>
2025-07-29 21:29:03 +0530

    deployment try 2

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://ordersathi.onrender.com', // Change this to your backend API URL
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  },
 })
